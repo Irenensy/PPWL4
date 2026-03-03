66789e633519becf8740cbb453a6cd947d064ce8
Irenensy <h1101241031@student.untan.ac.id>
2026-03-03 18:36:12 +0700

    news

---
+++ src/middleware.ts
@@ -0,0 +1,118 @@
+import { Elysia } from "elysia"
+import { openapi } from "@elysiajs/openapi";
+import {     t } from "elysia";
+
+const app = new Elysia()
+.use(openapi())
+// Global Logger
+app.onRequest(({ request }) => {
+ console.log("📥", request.method, request.url)
+ console.log("🕒", new Date().toISOString())
+})
+
+app.onRequest(({ request, set }) => {
+  if (request.headers.get("x-block") === "true") {
+    set.status = 403
+    return { message: "Blocked" }
+  }
+})
+
+app.get("/", () => "Hello Middleware")
+
+app.get(
+ "/dashboard",
+ () => ({
+   message: "Welcome to Dashboard"
+ }),
+ {
+   beforeHandle({ headers, set }) {
+     if (!headers.authorization) {
+       set.status = 401
+       return {
+         success: false,
+         message: "Unauthorized"
+       }
+     }
+   }
+ }
+)
+app.get(
+ "/admin",
+ () => ({
+   message: "Welcome to Dashboard"
+ }),
+ {
+   beforeHandle({ headers, set }) {
+     if (headers.authorization !== "Bearer 123") {
+       set.status = 401
+       return {
+         success: false,
+         message: "Unauthorized"
+       }
+     }
+   }
+ }
+)
+
+app.onAfterHandle(({ response }) => {
+ return {
+   success: true,
+   data: response
+ }
+})
+
+
+app.get("/profile", () => ({
+ name: "Nama kamu"
+}))
+
+app.get("/product", () => ({
+  id: 1,
+  name: "Laptop"
+}));
+
+
+
+app.post(
+ "/register",
+ ({ body }) => body,
+ {
+   body: t.Object({
+     name: t.String({ minLength: 3 }),
+     email: t.String({ format: "email" })
+   })
+ }
+)
+
+app.onError(({ code, error, set }) => {
+
+
+ if (code === "VALIDATION") {
+   set.status = 400
+   return {
+     success: false,
+     message: "Validation Error",
+     detail: error.message
+   }
+ }
+
+
+ if (code === "NOT_FOUND") {
+   set.status = 404
+   return {
+     message: "Route not found"
+   }
+ }
+
+
+ set.status = 500
+ return {
+   message: "Internal Server Error"
+ }
+})
+
+
+
+
+app.listen(3000)
+console.log("Server running at http://localhost:3000")
---
+++ src/validation.ts
@@ -0,0 +1,80 @@
+import { Elysia, t } from "elysia";
+import { openapi } from "@elysiajs/openapi";
+
+const app = new Elysia()
+  .use(openapi())
+  .post("/request",
+    ({ body }) => {
+      return {
+        message: "Success",
+        data: body
+      }
+    },
+    {
+      body: t.Object({
+        name: t.String({ minLength: 3 }),
+        email: t.String({ format: "email" }),
+        age: t.Number({ minimum: 18 })
+      })
+    }
+  )
+
+    app.get(
+    "/search",
+    ({ query }) => query,
+    {
+      query: t.Object({
+        keyword: t.String(),
+        page: t.Optional(t.Number())
+      })
+    }
+  )
+
+    app.get(
+    "/user/:id",
+    ({ params }) => {
+      return {
+        userId: params.id
+      }
+    },
+    {
+      params: t.Object({
+        id: t.String()
+      })
+    }
+  )
+
+  app.get("/products/:id"
+    , (context) =>{context.params , context.query},
+    {
+      params: t.Object({
+        id: t.Number()
+      }),
+      query: t.Object({
+        sort: t.String({ enum: ["asc", "desc"]})
+      })
+    }
+  )
+    app.get(
+    "/ping",
+    () => {
+      return {
+        success: true,
+        message: "Server OK"
+      }
+    },
+    {
+      response: t.Object({
+        success: t.Boolean(),
+        message: t.String()
+      })
+    }
+  )
+
+
+
+
+  .listen(3000);
+
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
