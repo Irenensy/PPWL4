3638dccea9461229befec77aa68fcf214320da61
Irenensy <h1101241031@student.untan.ac.id>
2026-03-04 13:53:12 +0700

    not

--- src/validation.ts
+++ src/validation.ts
@@ -87,22 +87,6 @@ const app = new Elysia()
   }
 )
 
-app.get(
-  "/product",
-  () => ({
-    id: 1,
-    name: "Laptop"
-  }),
-  {
-    afterHandle({ response }) {
-      return {
-        success: true,
-        Message: "data tersedia",
-        data: response
-      }
-    }
-  }
-)
 .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
